97aa7981f35a9f5efae5f209acc880955558ccc6
Robert Teets <teetsrobert@gmail.com>
2022-09-26 10:40:57 -0600

    init commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const pokémon = Object.freeze([
+    { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+    { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+    { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+    { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+    { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+    { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+    { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+    { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+    { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+    { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+    { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+    { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+    { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+    { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+    { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+    { "id": 98,  "name": "Krabby",     "types": ["water"] },
+    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+    { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+    { "id": 133, "name": "Eevee",      "types": ["normal"] },
+    { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+    { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+    { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+    { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+]);
+
+// id divisible by 3
+const div_by_3 = pokémon.filter( poke => poke.id % 3 === 0)
+
+// type is fire.
+const fire_types = pokémon.filter( poke => poke.types.includes('fire') )
+
+// more than one type
+const multi_types = pokémon.filter( poke => poke.types.length > 1 )
+
+// just the names
+const names = pokémon.map( poke => poke.name )
+
+// id greater than 99
+const greater_than_99 = pokémon.filter( poke => poke.id > 99 )
+
+// names with type of poison
+const names_with_poison = pokémon.filter( poke => poke.types.includes('poison')).map( poke => poke.name )
+
+// normal type, count
+const normal_types = pokémon.filter( poke => poke.types.includes('normal') ).length
+console.log(normal_types)
